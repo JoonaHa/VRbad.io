0db8086a9ce5de4f0f8915c3bd2e21c83f9bf093
Sami <saukkonensami@gmail.com>
2021-09-27 00:02:24 +0300

    Added a script that fetches data from digitraffic via graphql

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # VRbad.io
 Course projects for predicting if VR is bad using statistical methods
+
+# Setting up
+Make sure you are in the directory containing this README, and create a python virtual environment with command "python3 -m venv venv". Activate this virtual environment with "source venv/bin/activate". Load the required python packages with "pip install -r requirements.txt"
+
+# Fetching data
+Data can be fetched by running the python script fetchTrainData.py. The command requires output file name, and a GraphQL search query. These can be entered before command runs via terminal command "python fetchTrainData.py 'file_name' 'search_query'". The search query will be multiline, so make sure to wrap it with ''. Alternatively the command "python fetchTrainData.py" prompts the user for the output file name and the search query. After inserting the query via copy/paste, use ctrl+d to insert the data.
+
+The fetched data is saved into data -directory with the given name. All data saved is in .json format.
+
+Ready made queries can be found in queries.txt.
---
+++ fetchTrainData.py
@@ -0,0 +1,48 @@
+import sys
+import os
+import requests
+import json
+
+
+def get_user_inputs():
+  out_file = ""
+  query = ""
+  if len(sys.argv) == 3:
+    out_file = sys.argv[1]
+    query = sys.argv[2]
+  else:
+    out_file = input("Please insert output file name: ")
+    print("Please insert GraphQL query (examples here: https://www.digitraffic.fi/rautatieliikenne/#graphql)\n"
+    + "Finish giving input with ctrl+d")
+    query = "".join(sys.stdin.readlines())
+    print("")
+
+  if out_file != "" and query != "":
+    return (out_file, query)
+  else:
+    raise Exception("Given arguments need to be non-empty")
+
+
+def fetch_train_data(query):
+  endpoint = "https://rata.digitraffic.fi/api/v2/graphql/graphql"
+  headers = {"Content-Type": "application/json", "Accept-Encoding": "gzip"}
+
+  request = requests.post(endpoint, json={"query": query}, headers=headers)
+  if request.status_code == 200:
+      return request.json()
+  else:
+      raise Exception(f"Query failed to run with a {request.status_code}")
+
+def save_data_to_json(file, data):
+  if not os.path.isdir("data"):
+    os.makedirs("data")
+  
+  f = open(f"data/{file}.json", "w")
+  f.write(json.dumps(data, indent=2))
+  f.close()
+  print("Data fetching successful!")
+
+if __name__ == "__main__":
+  user_input = get_user_inputs()
+  train_data = fetch_train_data(user_input[1])
+  save_data_to_json(user_input[0], train_data)
